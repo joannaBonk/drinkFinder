ec627bec1a867cd38a17d21d925aafa2b85de376
Joanna <joanna.bonk08@gmail.com>
2022-08-12 08:26:29 +0200

    drink finder

---
+++ main.js
@@ -0,0 +1,31 @@
+const search = document.querySelector('.search')
+const li = document.querySelectorAll('li')
+
+
+const searchEngine = e => {
+    const text = e.target.value.toLowerCase()
+
+    li.forEach(el=> {
+        if(el.textContent.toLowerCase().indexOf(text) !== -1){
+            el.style.display = 'block'
+        } else {
+            el.style.display = 'none'
+        }
+    })
+}
+search.addEventListener('keyup', searchEngine)
+
+
+// Wyszukiwarka za pomocą RegExp
+
+// const searchRegExp = () => {
+//     li.forEach(el => {
+//         const match = new RegExp(search.value, 'i').test(el.textContent)
+//         if (!match) {
+//             el.style.display = 'none'
+//         } else
+//             el.style.display = 'block'
+//     })
+// }
+
+// search.addEventListener('keyup', searchRegExp)
